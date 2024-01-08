2cd28ec4c74326aff412521cc31cdcb3739688d8
gipsywontmakeit <8200689@estg.ipp.pt>
2024-01-08 18:15:57 +0000

    ENTITY, MESSAGE, SERVER

---
+++ src/Model/Entity.java
@@ -0,0 +1,67 @@
+package Model;
+
+import Enums.Rank;
+
+import java.util.List;
+
+public class Entity {
+    private int id;
+    private String username;
+    private String password;
+    private Rank rank;
+    private List<String> commsChannels;
+
+    public Entity(int id, String username, String password, Rank rank) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.rank = rank;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Rank getRank() {
+        return rank;
+    }
+    public List<String> getCommsChannels() {
+        return commsChannels;
+    }
+
+    public void addCommsChannels(String channel) {
+        commsChannels.add(channel);
+    }
+
+    public void removeCommsChannels(String channel) {
+        commsChannels.remove(channel);
+    }
+
+    public void sendMessage(String receiver, String message) {
+
+    }
+
+    public void requestHighTask(String description) {
+
+    }
+
+    public void acceptHighTaskRequest (String description) {
+
+    }
+
+    public void receiveMessageSeriousIncident(String message) {
+
+    }
+
+    public void beNotified(String message) {
+
+    }
+}
